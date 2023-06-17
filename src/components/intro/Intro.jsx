a49c6f10abb765b9596dee16d0cfe20dac9c7b2b
Nicholas Sotter <devsotter@gmail.com>
2023-06-17 01:58:19 -0300

    ajuste

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -7,7 +7,7 @@ export default function Intro() {
 
       <div className="right">
         <div className="wrapper">
-<<<<<<< HEAD
+
           <div className="texto">
             <h2>Hi There, I'm</h2>
             <h1>Carlos Edu</h1>
@@ -30,27 +30,7 @@ export default function Intro() {
           <div className="naosei">
             <img className="avatar" src="./assets/avatar.png" alt="pixel arte" />
           </div>
-=======
-       <div className="texto">
-       <h2>Hi There, I'm</h2>
-          <h1>Carlos Edu</h1>
-      <p>I'm an 19 years old Brazilian, have studied illustration and pixel art since 2019.
-Today, I am a character designer, cartoonist, and pixel artist, working in animating sprites for 2D pixel games.</p>
-<h3>Pixel Artist / Illustrator / Animator / Character Designer</h3>
-<div className="icones-redes">
-<a href="https://twitter.com/4piih"><Twitter/></a>
-<a href="https://www.facebook.com/4Piih/"><Facebook/></a>
-<a href="https://www.instagram.com/4piih/"><Instagram/></a>
-<a href="https://www.twitch.tv/4piih"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" fill="#fff" height="24px"><path d="M 5.3632812 2 L 2 6.6367188 L 2 20 L 7 20 L 7 23 L 10 23 L 13 20 L 17 20 L 22 15 L 22 2 L 5.3632812 2 z M 6 4 L 20 4 L 20 13 L 17 16 L 12 16 L 9 19 L 9 16 L 6 16 L 6 4 z M 11 7 L 11 12 L 13 12 L 13 7 L 11 7 z M 16 7 L 16 12 L 18 12 L 18 7 L 16 7 z"/></svg></a>
-</div>
-<a href="#portfolio">
-         <ArrowDropDown className="arrow"/>
-        </a>
-       </div>
-       <div className="naosei">
-  <img className="avatar" src="./assets/avatar.png" alt="" />
-  </div>
->>>>>>> a42fa862a6fdffb3d70bf0a0b9cf590ecaf14434
+
 
         </div>
 
--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-=======
 
-import ReactDOM from 'react-dom';
-import App from './App';
-import React from 'react';
-
->>>>>>> a42fa862a6fdffb3d70bf0a0b9cf590ecaf14434
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-<<<<<<< HEAD
   </React.StrictMode>,
   document.getElementById('root')
 );
-=======
-  </React.StrictMode>
-);
-
->>>>>>> a42fa862a6fdffb3d70bf0a0b9cf590ecaf14434
