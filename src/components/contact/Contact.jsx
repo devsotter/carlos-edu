049d2c0db503f0e038e6e794dfbced96a4d84a9f
Nicholas Sotter <devsotter@gmail.com>
2023-06-19 21:23:23 -0300

    ajuste

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,21 +3,17 @@ import "./contact.scss";
 export default function Contact() {
   return (
     <div className="contact" id="contact">
-      <div className="tito">
-        <h1>contact</h1>
-      </div>
-      <div className="con">
-        <div className="box-contato">
-          <div className="thank">
-            <img className="shake" src="assets/shake.svg" alt="ícone aperto de mãos" />
-            <div className="txt-thank">
-              <h2>Thank You</h2>
-              <p>let's work together?</p>
-              <a href="mailto:carlos.4piih@gmail.com">carlos.4piih@gmail.com</a>
-            </div>
-          </div>
+      <h1>Contact</h1>
+      <div className="container-contact">
+        <img src="assets/shake.svg" alt="ícone aperto de mãos" />
+        <div className="txt-contact">
+          <h2>Thank You</h2>
+          <p>let's work together?</p>
+          <a href="mailto:carlos.4piih@gmail.com">carlos.4piih@gmail.com</a>
+
         </div>
       </div>
+      <div className="copy"><p>Feito à base de cafeína por <a href="https://github.com/devsotter" target="_blank" rel="noopener noreferrer">Nicholas Sotter</a>.</p></div>
     </div>
   );
 }
